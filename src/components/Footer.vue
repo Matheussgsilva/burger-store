e10859abad42adb1bb75474bd42e0d1f9b19e030
Matheussgsilva <matheussgsilva@hotmail.com>
2022-07-06 18:22:12 -0300

    Finish the footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,24 @@
 <template>
-    <p>Footer</p>
+    <footer id="footer">
+        <p>Make your Burger &copy; 2022</p>
+    </footer>
 </template>
 <script>
     export default {
         name: 'Footer'
     }
 </script>
-<style>
-
+<style scoped>
+    #footer {
+        position: fixed;
+        height: 100px;
+        background-color: #222;
+        border-top: 4px solid #111;
+        color: #FCBA03;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        bottom: 0;
+        width: 100%;
+    }
 </style>
