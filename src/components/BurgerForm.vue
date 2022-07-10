2bb470f2713e6a25a85fea4a7f64b419dc89f154
Matheussgsilva <matheussgsilva@hotmail.com>
2022-07-10 16:57:58 -0300

    Create data and methods

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -2,7 +2,7 @@
     <div>
         <p>Componente de mensagem</p>
         <div>
-            <form id="burger-form">
+            <form id="burger-form" @submit="createBurger">
                 <div class="input-container">
                     <label for="name">Nome do cliente:</label>
                     <input type="text" id="name" class="name" v-model="name" placeholder="Digite o seu nome"/>
@@ -11,21 +11,21 @@
                     <label for="bread">Escolha o pão:</label>
                     <select name="bread" id="bread" v-model="bread">
                         <option value="">Esolha o seu pão</option>
-                        <option value="integral">Integral</option>
+                        <option v-for="bread in breadData" :key="bread.id" :value="bread.tipo">{{ bread.tipo }}</option>
                     </select>
                 </div>
                 <div class="input-container">
                     <label for="meat">Escolha a carne do seu burger:</label>
                     <select name="meat" id="meat" v-model="meat">
                         <option value="">Esolha o tipo de carne</option>
-                        <option value="maminha">Maminha</option>
+                        <option v-for="meat in meatData" :key="meat.id" :value="meat.tipo">{{ meat.tipo }}</option>
                     </select>
                 </div>
                 <div id="optional-container" class="input-container">
-                    <label id="optional-title" for="optional">Selecione os opicionais:</label>
-                    <div class="checkbox-container">
-                        <input type="checkbox" name="optional" v-model="optional" value="salame">
-                        <span>Salame</span>
+                    <label id="optional-title" for="optionals">Selecione os opicionais:</label>
+                    <div class="checkbox-container" v-for="optional in optionalData" :key="optional.id">
+                        <input type="checkbox" name="optionals" v-model="optionals" :value="optional.tipo">
+                        <span>{{ optional.tipo }}</span>
                     </div>
                 </div>
                 <div class="input-container">
@@ -38,6 +38,57 @@
 <script>
     export default {
         name: 'BuergerForm',
+        data() {
+            return {
+                breadData: null,
+                meatData: null,
+                optionalData: null,
+                name: null,
+                bread: null,
+                meat: null,
+                optionals: [],
+                msg: null
+            }
+        },
+        methods: {
+            async getIngredients() {
+                const req = await fetch('http://localhost:3000/ingredientes')
+                const data = await req.json()
+
+                this.breadData = data.paes
+                this.meatData = data.carnes
+                this.optionalData = data.opcionais
+            },
+            async createBurger(e) {
+                e.preventDefault()
+
+                const data = {
+                    name: this.name,
+                    meat: this.meat,
+                    bread: this.bread,
+                    optionals: Array.from(this.optionals),
+                    status: 'Solicitado'
+                }
+
+                const dataJson = JSON.stringify(data)
+
+                const req = await fetch('http://localhost:3000/burgers', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: dataJson
+                })
+
+                const res = await req.json()
+
+                this.bread = ''
+                this.meat =''
+                this.optionals = ''
+                this.name = ''
+            }
+        },
+        mounted() {
+            this.getIngredients()
+        }
     }
 </script>
 <style scoped>
