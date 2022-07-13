3c268a7de0238bf0a437c2937f92e0bc65c2aece
Matheussgsilva <matheussgsilva@hotmail.com>
2022-07-13 18:53:51 -0300

    Create requests methods and render them in table

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -11,20 +11,23 @@
             </div>
         </div>
         <div id="burger-table-rows">
-            <div class="burger-table-row">
-                <div class="order-number">1</div>
-                <div>Matheus</div>
-                <div>Integral</div>
-                <div>Picanha</div>
+            <div class="burger-table-row" v-for="burger in burgerOrders" :key="burger.id">
+                <div class="order-number">{{ burger.id }}</div>
+                <div>{{ burger.name }}</div>
+                <div>{{ burger.bread}}</div>
+                <div>{{ burger.meat}}</div>
                 <div>
                     <ul>
-                        <li>Bacon</li>
-                        <li>Queijo</li>
+                        <li v-for="(optional, index) in burger.optionals" :key="index">
+                            {{ optional }}
+                        </li>
                     </ul>
                 </div>
                 <div>
                     <select name="status" class="status">
-                        <option value="">Selecione</option>
+                        <option v-for="orderStatus in status" :key="orderStatus.id" :value="orderStatus.tipo" :selected="request.status === orderStatus.tipo">
+                            {{ orderStatus.tipo }}
+                        </option>
                     </select>
                     <button class="delete-btn">Cancelar</button>
                 </div>
@@ -34,7 +37,31 @@
 </template>
 <script>
     export default {
-        name: 'Dashboard'
+        name: 'Dashboard',
+        data() {
+            return {
+                burgerOrders: null,
+                status: [],
+            }
+        },
+        methods: {
+            async getRequests() {
+                const req = await fetch('http://localhost:3000/burgers')
+                const data = await req.json()
+
+                this.burgerOrders = data
+            },
+            async getStatus() {
+                const req = await fetch('http://localhost:3000/status')
+                const data = await req.json()
+
+                this.status = data
+            },
+        },
+        mounted() {
+            this.getRequests(),
+            this.getStatus
+        }
     }
 </script>
 <style scoped>
